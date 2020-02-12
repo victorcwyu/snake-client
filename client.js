03625b459edda96824d962a1120964a0f3bdfae6
victorcwyu <victorcwyu@gmail.com>
2020-02-12 19:49:01 +0000

    Created a constants module

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
+
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '172.46.0.236',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
