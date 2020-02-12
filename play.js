a313eba02af07d80a15b9fceae2f5c78966ca719
victorcwyu <victorcwyu@gmail.com>
2020-02-12 17:44:35 +0000

    Setup a client for snake game

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function () {
+  const conn = net.createConnection({
+    host: '172.46.0.236',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
